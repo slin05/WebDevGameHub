bcd3f6a013a6a9f792dcec2a2ef116670795c065
slin05 <97693822+slin05@users.noreply.github.com>
2025-03-31 16:49:29 -0400

    Basic Files Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
+import TicTacToe from './pages/tictactoe/tictactoe';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <h1>GameHub</h1>
-      <p>Welcome to GameHub</p>
-    </>
-  )
+    <Router>
+      <div className="app">
+        <Navigation />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* You'll need to add your game components here */}
+            
+            <Route path="/tic-tac-toe" element={<TicTacToe />} />
+            <Route path="/tic-tac-toe" element={<div>Tic Tac Toe Game</div>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
