204181e56451d803c1e407de029d1f48c12a82d5
slin05 <97693822+slin05@users.noreply.github.com>
2025-04-30 10:08:10 -0400

    Fixed Page Loading?

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/WebDevGameHub">
       <div className="app">
         <Navigation />
         <main className="main-content">
