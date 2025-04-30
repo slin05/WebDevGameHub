ae563975ac0e88126d035231a5280ebf56cdce7b
slin05 <97693822+slin05@users.noreply.github.com>
2025-04-30 15:28:36 -0400

    Trying to fix CSS

--- src/pages/black-jack/BlackjackGame.jsx
+++ src/pages/black-jack/BlackjackGame.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BlackjackTable from './BlackjackTable';
-import './Blackjack.css'
+import '/src/pages/black-jack/Blackjack.css';
 
 const App = () => {
     return (
